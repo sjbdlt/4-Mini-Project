ec2b4499a7d0331fb682184accdbcb0804ce3cf5
sjbdlt <141295016+sjbdlt@users.noreply.github.com>
2023-09-15 17:56:18 -0700

    final layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,161 @@
-var timeleft = document.getElementById("timer").innerHTML
-var currentword = ''
+var fillinword = document.querySelector("#word");
+var timedisplay = document.querySelector("#timer");
+var wins = document.querySelector("#lblwin");
+var losses = document.querySelector("#lblloss");
+var start = document.querySelector("#startgame");
+
+var currentword = "";
+var countBlanks = 0;
+var wCount = 0;
+var lCount = 0;
+var isWin = false;
+var timer;
+var tCount;
+
+
+var LettersinWord = [];
+var blanks = [];
+
+var currentwordarray = ['function', 'array', 'div', 'span', 'javascript'];
 
 function inti(){
-
-    var start = 0 //localStorage.getItem(startnumber);
-    var currentwordarray = ['FUNCTION', 'ARRAY', 'DIV', 'SPAN', 'JAVASCRIPT']
-        
-        if (start == 0){
-            currentword = currentwordarray[0]
-        }else if (start = 1) {
-            currentword = currentwordarray[1]
-        }else if (start = 2) {
-            currentword = currentwordarray[2]
-        }else if (start = 3) {
-            currentword = currentwordarray[3]
-        }else {
-            currentword = currentwordarray[4]
-        }           
-        setTime(); 
+  currentwins;
+  currentloses;
 }
+
+function startgame() {
+  isWin = false;
+  tCount = 15;
+  start.disabled = true;
+  setwordblanks();
+  settime();
+}
+
+
+function setwordblanks() {
+  currentword = currentwordarray[Math.floor(Math.random() * currentwordarray.length)];
+  LettersinWord = currentword.split("");
+  countBlanks = LettersinWord.length;
+  blanks = []
+  for (var i = 0; i < countBlanks; i++) {
+    blanks.push("_");
+  }
+  fillinword.value = blanks.join(" ")
+}
+
+
+function settime() {
+  timer = setInterval(function() {
+    tCount--;
+    timedisplay.textContent = tCount;
+    if (tCount >= 0) {      
+      if (isWin && tCount > 0) {
+        clearInterval(timer);
+        winGame();
+      }
+    }
+    if (tCount === 0) {
+      clearInterval(timer);
+      loseGame();
+    }
+  }, 1000);
+}
+
+function winGame() {
+  fillinword.value = "You win";
+  wCount++
+  start.disabled = false;
+  setWins()
+}
+
+function loseGame() {
+  fillinword.value = "You lose";
+  lCount++
+  start.disabled = false;
+  setLosses()
+}
+
+function setWins() {
+  wins.textContent = wCount;
+  localStorage.setItem("winCount", wCount);
+}
+
+function setLosses() {
+  losses.textContent = lCount;
+  localStorage.setItem("loseCount", lCount);
+}
+
+function currentwins() {
+   var strWins = localStorage.getItem("winCount");
+  if (strWins === null) {
+    wCount = 0;
+  } else {
+    wCount = strWins;
+  }
+  wins.textContent = wCount;
+}
+
+function currentloses() {
+  var strLosses = localStorage.getItem("loseCount");
+  if (strLosses === null) {
+    loseCounter = 0;
+  } else {
+    loseCounter = strLosses;
+  }
+  lose.textContent = strLosses;
+}
+
+function checkResults() {
+  if (currentword === blanks.join("")) {
+    isWin = true;
+  }
+}
+
+
+function checkLetters(letter) {
+  var letterInWord = false;
+  for (var i = 0; i < countBlanks; i++) {
+    if (currentword[i] === letter) {
+      letterInWord = true;
+    }
+  }
+  if (letterInWord) {
+    for (var j = 0; j < countBlanks; j++) {
+      if (currentword[j] === letter) {
+        blanks[j] = letter;
+      }
+    }
+    fillinword.value = blanks.join(" ");
+  }
+}
+
+document.addEventListener("keydown", function(event) {
+  if (tCount === 0) {
+    return;
+  }
+  var key = event.key.toLowerCase();
+  var alphabetNumericCharacters = "abcdefghijklmnopqrstuvwxyz ".split("");
+  if (alphabetNumericCharacters.includes(key)) {
+    var letterGuessed = event.key;
+    checkLetters(letterGuessed)
+    checkResults();
+  }
+});
+
+start.addEventListener("click", startgame);
 
 inti();
 
 
-window.addEventListener('keydown', function (event) {
-  // Access value of pressed key with key property
-  var key = event.key.toLowerCase();
-  var wordCharacters = currentword.split(
-    ''
-  );
-  if (wordCharacters.includes(key)) {
-    for (var i = 0; i < elements.length; i++) {
-      elements[i].textContent += event.key;
-    }
-  }
-});
+var resetBtn = document.querySelector("#resetgame");
 
-var tileft = 25
-function setTime() {
-    // Sets interval in variable
-    var timerInterval = setInterval(function() {
-    tileft--;
-      timeleft.textContent = tileft + " seconds till game over";
-  
-      if(tileft === 0) {
-        clearInterval(timerInterval);
-      }
-  
-    }, 1000);
-  }
+function newgame() {
+  wCount = 0;
+  lCount = 0;
+  setWins()
+  setLosses()
+}
+
+resetBtn.addEventListener("click", newgame);
+
+
